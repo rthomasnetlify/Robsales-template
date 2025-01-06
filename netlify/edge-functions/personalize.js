97cba14ff33a07fa4de9e2950cda2a47641a18dc
rthomasnetlify <rob.thomas@netlify.com>
2025-01-06 11:31:25 +0000

    Update personalize.js

--- netlify/edge-functions/personalize.js
+++ netlify/edge-functions/personalize.js
@@ -1,10 +1,8 @@
 import { HTMLRewriter } from "https://ghuc.cc/worker-tools/html-rewriter/index.ts";
-
 export default async (request, context) => {
     const url = new URL(request.url);
     const response = await context.next();
     const location = context?.geo?.city;
-
     if (url.searchParams.get("method") === "personalize") {
         const rewriter = new HTMLRewriter()
             .on("#netlify-title", {
@@ -16,13 +14,10 @@ export default async (request, context) => {
                     }
                 },
             })
-
         return rewriter.transform(response);
     }
-
     return response;
 };
-
 export const config = {
     path: "/",
 };
